3f748402f7e0211303829f2990cdb4f9aa0d7e2b
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-21 21:07:15 +0530

    Add checkAttendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,4 +1,13 @@
+import java.util.Random;
+
 public class EmployeeWageComputation {
+
+    boolean checkAttendance() {
+        Random random = new Random();
+        int present = random.nextInt(2);
+        return (present == 1);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
     }
