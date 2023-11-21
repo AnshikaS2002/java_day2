a5f21d7c5de444318989b649cad1408ddf4f0b72
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-21 23:55:37 +0530

    added areEndpointsEqual methon

--- LineComparision.java
+++ LineComparision.java
@@ -1,6 +1,10 @@
 public class LineComparision {
-    static double calculateLineLength(double x1, double y1, double x2, double y2) {
+    double calculateLineLength(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    boolean areEndpointsEqual(double x1L1, double y1L1, double x2L1, double y2L1, double x1L2, double y1L2, double x2L2, double y2L2) {
+        return (x1L1 == x1L2 && y1L1 == y1L2 && x2L1 == x2L2 && y2L1 == y2L2);
     }
 
     public static void main(String[] args) {
