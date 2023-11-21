2f4a8d2b273041012aedda39f8ec3642c70c30cb
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-21 23:22:46 +0530

    added monthly wage function

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -58,6 +58,12 @@ public class EmployeeWageComputation {
         return wage;
     }
 
+    int monthlyWage() {
+        EmployeeWageComputation ewc = new EmployeeWageComputation();
+        int dailyWage = ewc.dailyWage();
+        return dailyWage * 20;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
     }
