9766f8cafee9ef545a16b512b2d780a9541c7726
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-21 23:47:37 +0530

    Added lineLength calculating function

--- LineComparision.java
+++ LineComparision.java
@@ -1,4 +1,8 @@
 public class LineComparision {
+    static double calculateLineLength(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to line comparision problem");
     }
